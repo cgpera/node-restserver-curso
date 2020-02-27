f188251804848584d144711a572bf60aa9908cfe
Claudio Pera <claudio@Claudios-iMac.local>
2020-02-27 10:29:15 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     //urlDB = 'mongodb+srv://cgpera:pelado00@cluster0-a4rjc.mongodb.net/cafe';
-    urlDB = 'mongodb+srv://cafe-user:ZxPcg42ZVPXCOMUq@cluster0-a4rjc.mongodb.net/cafe';
+    urlDB = 'mongodb+srv://cafe-user:zQV6hRjWVh3kAcGL@cluster0-a4rjc.mongodb.net/cafe';
 }
 process.env.URLDB = urlDB;
