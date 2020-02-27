115072fa709f1b6a557094fe6701632cfb6ec3ef
Claudio Pera <claudio@Claudios-iMac.local>
2020-02-27 16:56:57 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    //urlDB = 'mongodb+srv://cgpera:pelado00@cluster0-a4rjc.mongodb.net/cafe';
-    urlDB = 'mongodb+srv://cafe-user:zQV6hRjWVh3kAcGL@cluster0-a4rjc.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
