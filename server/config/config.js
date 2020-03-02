0d0d4d856c3150dcf28d680a7f1bc42e0e8d680d
Claudio Pera <cgpera@gmail.com>
2020-03-02 14:24:30 -0300

    personalizacion de produccion

--- server/config/config.js
+++ server/config/config.js
@@ -9,6 +9,27 @@ process.env.PORT = process.env.PORT || 3000
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // ===========================
+// Vencimiento del token
+// ===========================
+// 60 seg
+// 60 min
+// 24 horas
+// 30 días
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// ===========================
+// SEED
+// ===========================
+// SEED_PROD es hecha con config de heroku
+if (process.env.NODE_ENV === 'dev') {
+    SEED = 'Este-es-el-seed-desarrollo';
+} else {
+    SEED = process.env.SEED_PROD;
+}
+process.env.SEED = SEED;
+
+
+// ===========================
 // Base de datos
 // ===========================
 let urlDB;
