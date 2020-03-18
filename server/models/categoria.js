e95c8aa4ecd0eed8427ca738eb1cb41eaee7467e
Claudio Pera <cgpera@gmail.com>
2020-03-18 18:04:47 -0300

    categorias

---
+++ server/models/categoria.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator')
+
+let Schema = mongoose.Schema;
+
+let categoriaSchema = new Schema({
+    nombre: {
+        type: String,
+        unique: true,
+        required: [true, 'El nombre es obligatorio']
+    },
+    usuario: {
+        type: Schema.Types.ObjectId,
+        ref: 'Usuario'
+    }
+});
+
+
+categoriaSchema.plugin(uniqueValidator, { message: '{PATH} debe ser único' });
+
+module.exports = mongoose.model('Categoria', categoriaSchema)
